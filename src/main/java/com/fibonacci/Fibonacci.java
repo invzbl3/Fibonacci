7b4c39993cb54cee11720b205690e0559b33f96a
Maksym Rohachov <romax982@gmail.com>
2022-10-21 16:42:52 +0200

    Update code

--- src/main/java/com/fibonacci/Fibonacci.java
+++ src/main/java/com/fibonacci/Fibonacci.java
@@ -1,3 +1,7 @@
+package com.fibonacci;
+
+import java.util.Arrays;
+
 /**
  * @author invzbl3 on 10/21/2022
  * @project Finbonacci
@@ -15,15 +19,21 @@ class Fibonacci {
 
     public static void main(String[] args) {
         int number = 21;
+
+        int[] fibo_numbers = new int [number];
+        Arrays.fill(fibo_numbers, 1); // [1, 1, 1, 1]
+
         for (int i = 0; i < number; i++) {
+            fibo_numbers[i]=fibonacci_numbers(i);
             System.out.print(fibonacci_numbers(i) + " ");
         }
 
-        System.out.println("\n\nFibonacci series for " + number +" numbers : ");
+        System.out.println("\n\nFibonacci series for " + number + " numbers : ");
         System.out.println("\n\nMean " + mean(number));
+        System.out.println("\n\nMedian " + median((fibo_numbers)));
     }
 
-    static float mean(int number) {
+    public static float mean(int number) {
         int fibo1 = 1, fibo2 = 1, sum = 0, average = 0, fibonacci;
         for (int i = 1; i <= 2 && i <= number; i++) {
             System.out.print("1 ");
@@ -37,5 +47,14 @@ class Fibonacci {
             sum += fibonacci;
         }
         return (float)sum/number;
+    }
+
+    public static double median(int[] m) {
+        int middle = m.length / 2;
+        if (m.length % 2 == 1) {
+            return m[middle];
+        } else {
+            return (m[middle - 1] + m[middle]) / 2;
+        }
     }
 }
